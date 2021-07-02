b8c89bf2e890bc3831178e4d1577b0c5b9969f0f
Pedro Enciso <penciso@ucsd.edu>
2021-07-01 22:08:31 -0700

    unique names for each data entry

--- data/photographyData.js
+++ data/photographyData.js
@@ -1,20 +1,20 @@
 export const photographyData = [
   {
-    name: "Cal Poly Pamona Athletics",
+    name: "Cal Poly Pamona Athletics 01",
     date: "Fall 2019 - Spring 2020",
     coverImage: "/../public/images/IMG_9049.jpg",
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
   {
-    name: "Cal Poly Pamona Athletics",
+    name: "Cal Poly Pamona Athletics 02",
     date: "Fall 2019 - Spring 2020",
     coverImage: "/../public/images/IMG_9049.jpg",
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
   {
-    name: "Cal Poly Pamona Athletics",
+    name: "Cal Poly Pamona Athletics 03",
     date: "Fall 2019 - Spring 2020",
     coverImage: "/../public/images/IMG_9049.jpg",
     color: "#2a423e",
