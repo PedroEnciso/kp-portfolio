dcb7056ac8cb1085d85b0977d05de899c9e17d18
Pedro Enciso <penciso@ucsd.edu>
2021-07-01 19:53:35 -0700

    created format for project data

---
+++ data/photographyData.js
@@ -0,0 +1,9 @@
+const photographyData = [
+  {
+    name: "Cal Poly Pamona Athletics",
+    date: "Fall 2019 - Spring 2020",
+    coverImage: "../public/images/",
+    color: "#2a423e",
+    gallery: ["first image", "second image", "third image", "fourth image"],
+  },
+];
