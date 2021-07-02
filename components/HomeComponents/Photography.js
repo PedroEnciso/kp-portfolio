4d7303d2ebdd31559220577cedb2347ca219f25e
Pedro Enciso <penciso@ucsd.edu>
2021-07-01 22:06:51 -0700

    photgraphyData rendered on photo tab

--- components/HomeComponents/Photography.js
+++ components/HomeComponents/Photography.js
@@ -1,10 +1,26 @@
+import styles from "../../styles/homeComponentStyles/photography.module.css";
+import { photographyData } from "../../data/photographyData";
+import Image from "next/image";
+
 export default function Photography() {
   return (
-    <div>
-      <section>
-        <h2>Photography</h2>
-        <p>This is a list of Kalea's photography projects</p>
-      </section>
+    <div className={styles.container}>
+      <div className={styles.projectGrid}>
+        {photographyData.map((project) => (
+          <div key={project.name} className={styles.projectContainer}>
+            <h3 className={styles.projectName}>{project.name}</h3>
+            <div className={styles.imageContainer}>
+              <Image
+                width={200}
+                height={250}
+                className={styles.coverImage}
+                src={project.coverImage}
+                alt="cover image"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- data/photographyData.js
+++ data/photographyData.js
@@ -1,8 +1,22 @@
-const photographyData = [
+export const photographyData = [
   {
     name: "Cal Poly Pamona Athletics",
     date: "Fall 2019 - Spring 2020",
-    coverImage: "../public/images/",
+    coverImage: "/../public/images/IMG_9049.jpg",
+    color: "#2a423e",
+    gallery: ["first image", "second image", "third image", "fourth image"],
+  },
+  {
+    name: "Cal Poly Pamona Athletics",
+    date: "Fall 2019 - Spring 2020",
+    coverImage: "/../public/images/IMG_9049.jpg",
+    color: "#2a423e",
+    gallery: ["first image", "second image", "third image", "fourth image"],
+  },
+  {
+    name: "Cal Poly Pamona Athletics",
+    date: "Fall 2019 - Spring 2020",
+    coverImage: "/../public/images/IMG_9049.jpg",
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
