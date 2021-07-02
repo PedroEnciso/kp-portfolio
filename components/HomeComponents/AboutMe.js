113712b1c874fdd47f591ca580551becee3b4518
Pedro Enciso <penciso@ucsd.edu>
2021-07-01 22:16:34 -0700

    trying to fix errors for vercel

--- components/HomeComponents/AboutMe.js
+++ components/HomeComponents/AboutMe.js
@@ -1,10 +1,8 @@
 export default function AboutMe() {
   return (
-    <div>
-      <section>
-        <h2>About Me</h2>
-        <p>This is Kalea's bio</p>
-      </section>
-    </div>
+    <section>
+      <h2>About Me</h2>
+      <p>This is Kalea's bio</p>
+    </section>
   );
 }
--- components/HomeComponents/Design.js
+++ components/HomeComponents/Design.js
@@ -1,10 +1,8 @@
 export default function Design() {
   return (
-    <div>
-      <section>
-        <h2>Design</h2>
-        <p>This is a list of Kalea's design projects</p>
-      </section>
-    </div>
+    <section>
+      <h2>Design</h2>
+      <p>This is a list of Kalea's design projects</p>
+    </section>
   );
 }
