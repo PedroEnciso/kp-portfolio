17bd68d93793c42394dae1fa352b12d9002b21d0
Pedro Enciso <penciso@ucsd.edu>
2021-07-03 10:41:46 -0700

    issue fixed, fixing everywhere

--- data/photographyData.js
+++ data/photographyData.js
@@ -1,24 +1,24 @@
-import image from "../public/images/IMG_9049.jpg";
+import image01 from "../public/images/IMG_9049.jpg";
 
 export const photographyData = [
   {
     name: "Cal Poly Pamona Athletics 01",
     date: "Fall 2019 - Spring 2020",
-    coverImage: image,
+    coverImage: image01,
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
   {
     name: "Cal Poly Pamona Athletics 02",
     date: "Fall 2019 - Spring 2020",
-    coverImage: "/../public/images/IMG_9049.jpg",
+    coverImage: image01,
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
   {
     name: "Cal Poly Pamona Athletics 03",
     date: "Fall 2019 - Spring 2020",
-    coverImage: "/../public/images/IMG_9049.jpg",
+    coverImage: image01,
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
