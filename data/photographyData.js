e07d14480c8a901ec05fc811b1389fe0e35c079d
Pedro Enciso <penciso@ucsd.edu>
2021-07-03 10:38:03 -0700

    trying to resolve errors 2.0

--- data/photographyData.js
+++ data/photographyData.js
@@ -1,8 +1,10 @@
+import image from "../public/images/IMG_9049.jpg";
+
 export const photographyData = [
   {
     name: "Cal Poly Pamona Athletics 01",
     date: "Fall 2019 - Spring 2020",
-    coverImage: "/../../public/images/IMG_9049.jpg",
+    coverImage: image,
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
