1da766b704ce034f9077e32cfa69502365cac505
Pedro Enciso <penciso@ucsd.edu>
2021-07-04 13:08:27 -0700

    added more data and responsive styles

--- data/photographyData.js
+++ data/photographyData.js
@@ -1,24 +1,26 @@
 import image01 from "../public/images/IMG_9049.jpg";
+import image02 from "../public/images/P1111328.jpg";
+import image03 from "../public/images/DSC05606.jpg";
 
 export const photographyData = [
   {
-    name: "Cal Poly Pamona Athletics 01",
+    name: "Cal Poly Pamona Athletics",
     date: "Fall 2019 - Spring 2020",
     coverImage: image01,
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
   {
-    name: "Cal Poly Pamona Athletics 02",
+    name: "PM3D",
     date: "Fall 2019 - Spring 2020",
-    coverImage: image01,
+    coverImage: image02,
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
   {
-    name: "Cal Poly Pamona Athletics 03",
+    name: "Andrew & Justine's Wedding",
     date: "Fall 2019 - Spring 2020",
-    coverImage: image01,
+    coverImage: image03,
     color: "#2a423e",
     gallery: ["first image", "second image", "third image", "fourth image"],
   },
